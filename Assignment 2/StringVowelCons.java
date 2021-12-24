536d45806348cefe301f5814da6fb88cb8506008
ShrinivasMalekar <92094051+ShrinivasMalekar@users.noreply.github.com>
2021-12-25 01:10:33 +0530

    Add files via upload

---
+++ Assignment 2/StringVowelCons.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StringVowelCons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a statement: ");
+        String s = sc.nextLine();
+        s = s.toLowerCase();
+
+        int v = 0, c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+                v++;
+            } else if (a >= 'a' && a <= 'z') {
+                c++;
+            }
+        }
+
+        System.out.println("Vowels: " + v);
+        System.out.println("Consonants: " + c);
+    }
+}
