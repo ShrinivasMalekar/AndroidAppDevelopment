7f285083873dbfe7be16d4a0aba4a623555931f7
ShrinivasMalekar <92094051+ShrinivasMalekar@users.noreply.github.com>
2021-12-25 00:32:09 +0530

    Add files via upload

---
+++ Assignment 2/MinArry.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MinArry
+{
+        public static void main(String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter 5 elements in an array");
+
+
+            int arr[] = new int[5];
+            for(int a=0; a<arr.length;a++)
+            {
+                arr[a] = sc.nextInt();
+            }
+            int x = arr[0];
+
+            for(int i=0;i<arr.length;i++)
+            {
+                if(arr[i]<x)
+                    x = arr[i];
+            }
+            System.out.println("Smallest element in Array is "+x);
+        }
+
+
+}
